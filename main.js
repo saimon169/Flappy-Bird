1e87f489d4055c6bfae92e283940ac6f376263a4
sujalpradhan <sujalof2001@gmail.com>
2023-11-04 20:04:42 +0545

    first commit

--- main.js
+++ main.js
@@ -17,6 +17,20 @@ let bird = {
     height : birdHeight
 }
 
+//variables for pipe
+let pipeArray=[];
+let pipeWidth=64;
+let pipeHeight=512;
+let pipeX=boardWidth;
+let pipeY=0;
+
+let topPipeImg;
+let bottomPipeImg;
+
+//physics 
+let velocityX=-2; //pipes moving left speed 
+
+
 window.onload = function() {
     board = document.querySelector('#board');
     board.height = boardHeight;
@@ -33,14 +47,45 @@ window.onload = function() {
     birdImage.onload = function(){
         context.drawImage(birdImage,bird.x,bird.y,bird.width,bird.height);
     }
+
+    topPipeImg=new Image();
+    topPipeImg.scr='./images/toppipe.png';
+
+    bottomPipeImg=new Image();
+    bottomPipeImg.scr='./images/bottompipe.png';
+
     requestAnimationFrame(update);
+    setInterval(placepipes, 1500); // 1.5 secs
+
 }
 
 //helps to keep the frames move in  loop
 function update() {
     requestAnimationFrame(update);
-    context.clearRect(0, 0, boardWidth, boardHeight);
+    context.clearRect(0, 0, board.width, board.height);
 
     //bird
     context.drawImage(birdImage,bird.x,bird.y,bird.width,bird.height);
+
+    //pipes
+    for (let i=0;i<pipeArray.length;i++){
+        let pipe=pipeArray[i];
+        pipe.x +=velocityX;
+        context.drawImage(pipe.img, pipe.x, pipe.y, pipe.width, pipe.height);
+    }
+}
+
+//every 1.5 sec yo function call 
+function placepipes(){
+    let topPipe={
+        img : topPipeImg,
+        x: pipeX,
+        y: pipeY,
+        width: pipeWidth,
+        height: pipeHeight,
+        passed: false
+    }
+
+    pipeArray.push(topPipe);    //add new pipe to array
+
 }
